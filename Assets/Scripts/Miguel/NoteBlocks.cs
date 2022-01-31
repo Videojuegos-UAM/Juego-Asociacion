80118d20ffc86f157c35ee98d01a439e7af14c31
MiguelFont <31103394+MiguelFont@users.noreply.github.com>
2022-01-31 18:49:45 +0100

    Branch Creation

---
+++ Assets/Scripts/Miguel/NoteBlocks.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBlocks : MonoBehaviour{
+    public Transform transformation;
+
+    // Start is called before the first frame update
+    void Start() {
+        transformation.position = ;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transformation.position =;
+    }
+}
